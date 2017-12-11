1ff7c3ac44da54214c248686decc83d1947fd661
mirkomikac <mmikac27@gmail.com>
2017-12-11 17:52:44 +0100

    "Initial commit"

---
+++ DataAccessService/src/main/java/com/sep/tim2/da/DataAccessServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sep.tim2.da;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAccessServiceApplication.class, args);
+	}
+}
